8c95164d60633032e31168e5a474a4ed7525ff47
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-12-09 22:36:58 +0100

    optimistion

--- theremine/theremine.ino
+++ theremine/theremine.ino
@@ -1,45 +1,44 @@
 /*
-  Entrée et sortie analogiques + communications série
- 
- Ce programme va faire emmettre des sons a un buzzer
- L'intensité du "buzz" sera proportionnelle à la luminosité
- captée par la photorésistance branchée sur la broche A2.
- 
- */
+Entrée et sortie analogiques + communications série
 
+Ce programme va émettre des sons à l'aide d'un buzzer branché sur la broche 8.
+Le son émit par le buzzer sera proportionnel à la luminosité
+captée par la photorésistance branchée sur la broche A2.
+
+*/
 // Initialisation des constantes :
-const int analogInPin = A2;   // Numéro de la broche à laquelle est connecté la photorésistance
-const int analogOutPin = 8;   // Numéro de la broche à laquelle est connectée le buzzer
-
-int sensorValue = 0;          // Valeur lue sur la photorésistance
-int outputValue = 0;          // Valeur envoyée au buzzer
-
+const int brocheCapteur=A2; // Numéro de la broche à laquelle est connecté la photorésistance
+const int brocheBuzzer=8; // Numéro de la broche à laquelle est connectée le buzzer
+int valeurCapteur=0; // Valeur lue sur la photorésistance
+int valeurSortieBuzzer=0; // Valeur envoyée au buzzer
 void setup() {
-  // Initialise la communication avec l'ordinateur
-  Serial.begin(9600);
-  
-  // Indique que la broche analogOutPin est une sortie :
-  pinMode(analogOutPin, OUTPUT);      
-  // Indique que la broche analogInPin est une entrée :
-  pinMode(analogInPin, INPUT);     
+// Initialise la communication avec l'ordinateur
+Serial.begin(9600);
+// Indique que la broche brocheCapteur est une entrée :
+pinMode(brocheCapteur, INPUT);
+// Indique que la broche brocheBuzzer est une sortie :
+pinMode(brocheBuzzer, OUTPUT);
+
 }
 
 void loop() {
-  // lit la valeur de la photorésistance et
-  // stocke le résultat dans sensorValue :
-  sensorValue = analogRead(analogInPin);            
-  // change sensorValue vers une intervalle de 0 à 255
-  // et stocke le résultat dans outputValue :
-  outputValue = map(sensorValue, 0, 1023, 50, 30000);  
-  //FrkBuz = map(PRValue, 0, 1023, 50, 30000);
-  // envoie de cette nouvelle valeur sur le buzzer
-  analogWrite(analogOutPin, outputValue);           
+// lit la valeur de la photorésistance et
+// stocke le résultat dans valeurCapteur :
+valeurCapteur=analogRead(brocheCapteur);
+// change valeurCapteur vers une intervalle de 50 à 30000
+// et stocke le résultat dans valeurSortieBuzzer :
+valeurSortieBuzzer=map(valeurCapteur, 0, 1024, 50, 30000);
+// envoie de cette nouvelle valeur de valeurSortieBuzzer sur le buzzer
+// ce qui permetra d'émettre du son
+tone(brocheBuzzer, valeurSortieBuzzer);
+// envoie les valeur vers l'ordinateur
+// juste pour afficher les valeur dans
+// le moniteur serie
+Serial.print("valeur entrée capteur = ");
+Serial.print(valeurCapteur);
+Serial.print("\t valeur sortie Buzzer = ");
+Serial.println(valeurSortieBuzzer);
+//attendre 2 millisecondes pour permettre au son de se moduler
+delay(2);
 
-  // envoie tout ça vers l'ordinateur
- 
-  Serial.print("sensor = " );                       
-  Serial.print(sensorValue);      
-  Serial.print("\t output = ");      
-  Serial.println(outputValue);
-  
 }
