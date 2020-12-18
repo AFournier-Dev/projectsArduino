f4537f9292172d0be2113d6565f3103a19017dbb
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-12-18 23:19:26 +0100

    refactoriser le code encore des bugs mais progres grace à la reprise intégrale du code Warning BUG WIP

--- feuxBicolorePlusBarriere/feuxBicolorePlusBarriere.ino
+++ feuxBicolorePlusBarriere/feuxBicolorePlusBarriere.ino
@@ -3,36 +3,41 @@
 Servo myservo;  // créer un objet appelé myservo à partir
 // du moule Servo
 
-int pos = 90;    // variable pour stocker la position courante du servo
-const int rouge = 3;
-const int verte = 4;
-const int bouton = 2;  //Le bouton est connecté à la PIN 2
-int etatbouton = 0;
+int pos = 0;    // variable pour stocker la position courante du servo
+const int buttonPin = 2;     // Numéro de la broche à laquelle est connecté le bouton poussoir
+int buttonState = 0;         // variable qui sera utilisée pour stocker l'état du bouton
 void setup() 
 { 
-  Serial.begin(9600);
   myservo.attach(9);  // attacher notre objet myservo au servomoteur branché sur la broche 9 
-  pinMode(rouge, OUTPUT); // Défintion des entrées/sorties
-  pinMode(verte, OUTPUT);
-  pinMode(bouton, INPUT);
+  
+  pinMode(buttonPin, INPUT);  // indique que la broche buttonPin est une entrée
+  
+  pinMode(4, OUTPUT); // LED VERTE
+  pinMode(3, OUTPUT); // LED ROUGE
+  digitalWrite(3, HIGH); // Allume la led Rouge à l'initialisation
 } 
 
 
 void loop() 
 { 
-   digitalWrite(rouge, HIGH);
-  digitalWrite(verte, LOW);
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == HIGH){
+    for(pos = 0; pos <= 90; pos += 1){  // aller de 0° à 179°// une étape à la fois
+      myservo.write(pos);              // aller à la position stocké dans 'pos'
+      delay(15);                       // attendre 15ms que le servomoteur se rende à 'pos'
+    } 
+    digitalWrite(3, LOW); // eteindre LED rouge  
+    digitalWrite(3, HIGH);// allumer la verte
+  delay(5000); // attendre 5s
+    digitalWrite(3, HIGH); // allumer la rouge
 
-
-
-  for(pos = 90; pos < 180; pos += 1)  // aller de 0° à 179°
-  {                                  // une étape à la fois
+    
+    for(pos = 90; pos>=0; pos-=1){     /// aller de 180° à 1°
     myservo.write(pos);              // aller à la position stocké dans 'pos'
-    delay(5);                       // attendre 15ms que le servomoteur se rende à 'pos'
-  } 
-  for(pos = 180; pos>=90; pos-=1)     /// aller de 180° à 1°
-  {                                
-    myservo.write(pos);              // aller à la position stocké dans 'pos'
-    delay(15);                       // attendre 15ms que le servomoteur se rende à 'pos'
-  } 
+      delay(15);                       // attendre 15ms que le servomoteur se rende à 'pos'
+    } 
+  }
+  
+  
 }  
